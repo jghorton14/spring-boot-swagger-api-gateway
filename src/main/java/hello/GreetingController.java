9c50e9d6cbd021998e889188ed01e1778d00e7a2
Jarrett Horton <jghorton14@gmail.com>
2020-10-16 20:53:35 -0400

    add

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,26 +1,15 @@
 package hello;
 
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
 
-  private static final String template = "Hello %s!";
-  private final AtomicLong counter = new AtomicLong();
-
-  @RequestMapping("/")
-  public String home() {
-    System.out.println("Home Page");
-    return "Home";
+  @RequestMapping(value = "/greeting", method = RequestMethod.GET, produces = "text/plain")
+  public String greeting(@RequestParam(value = "value", defaultValue = "World") String name) {
+    return name;
   }
-
-  @RequestMapping("/greeting")
-  public Greeting greeting(@RequestParam(value = "value", defaultValue = "World") String name) {
-    System.out.println("Template Name: " + String.format(template, name));
-    return new Greeting(counter.incrementAndGet(), String.format(template, name));
-  }
-
 }
